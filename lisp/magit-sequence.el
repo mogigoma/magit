fe47ba26c7259e4a0f3bc49536cab4619cb3b7d9
Jonas Bernoulli <jonas@bernoul.li>
2018-10-15 11:59:00 +0200

    Define magit-patch-apply as a transient command

--- lisp/magit-sequence.el
+++ lisp/magit-sequence.el
@@ -387,7 +387,7 @@ without prompting."
   :options  '((?p "Remove leading slashes from paths" "-p" magit-read-number))
   :actions  '((?m "Apply maildir"     magit-am-apply-maildir)
               (?w "Apply patches"     magit-am-apply-patches)
-              (?a "Apply plain patch" magit-patch-apply-popup))
+              (?a "Apply plain patch" magit-patch-apply))
   :default-arguments '("--3way")
   :default-actions 'magit-am-apply-patches
   :max-action-columns 1
